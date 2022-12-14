1c8d22c4b05629dfdcf0f2ccb94e6e8f639954d3
charming-c <822170415@qq.com>
2022-12-14 17:05:44 +0800

    handbook page

--- covid-control/pages/recoverymanual/recoverymanual.vue
+++ covid-control/pages/recoverymanual/recoverymanual.vue
@@ -1,6 +1,8 @@
 <template>
-	<view>
-		
+	<view style="width: 100%; height: 100%;">
+		<u-image :fade="true" height="100%" duration="450" src="https://s2.loli.net/2022/12/14/qHbOuS9UCckhnp5.png" mode="widthFix">
+			<u-loading  mode="flower" size="36" slot="loading"></u-loading>
+		</u-image>
 	</view>
 </template>
 
@@ -18,5 +20,5 @@
 </script>
 
 <style>
-
+	
 </style>
