3560573033c209fa0abb3394fdc577ea76402bdc
drowning-in-codes <bukalala174@gmail.com>
2022-12-14 22:23:57 +0800

    增加可视化结果 除水球图之外

--- covid-control/pages/predictionresult/predictionresult.vue
+++ covid-control/pages/predictionresult/predictionresult.vue
@@ -1,120 +1,350 @@
 <template>
-	<view>
+	<view class="result-container">
 		<view class="echart_panel1">
 			<l-echart ref="chart1" @finished="chart1init"></l-echart>
 		</view>
 		<view class="echart_panel2">
 			<l-echart ref="chart2" @finished="chart2init"></l-echart>
 		</view>
-
+		<view class="echart_panel3">
+			<l-echart ref="chart3" @finished="chart3init"></l-echart>
+		</view>
+		<button class="back-button" @click="toHome">返回首页</button>
 	</view>
 </template>
 
 <script>
-	import * as echarts from '@/uni_modules/lime-echart/static/echarts'
+	import * as echarts from '@/uni_modules/lime-echart/static/echarts.min';
+	// import * as liquidFill from '@/uni_modules/lime-echart/static/echarts-liquidfill.min.js';
 	export default {
 		data() {
 			return {
-				option1: {
-					tooltip: {
-						trigger: 'axis',
-						axisPointer: {
-							type: 'shadow'
-						},
-						confine: true
-					},
-					legend: {
-						data: ['热度', '正面', '负面']
-					},
-					grid: {
-						left: 20,
-						right: 20,
-						bottom: 15,
-						top: 40,
-						containLabel: true
-					},
-					xAxis: [{
-						type: 'value',
-						axisLine: {
-							lineStyle: {
-								color: '#999999'
-							}
-						},
-						axisLabel: {
-							color: '#666666'
-						}
-					}],
-					yAxis: [{
-						type: 'category',
-						axisTick: {
-							show: false
-						},
-						data: ['汽车之家', '今日头条', '百度贴吧', '一点资讯', '微信', '微博', '知乎'],
-						axisLine: {
-							lineStyle: {
-								color: '#999999'
-							}
-						},
-						axisLabel: {
-							color: '#666666'
-						}
-					}],
-					series: [{
-							name: '热度',
-							type: 'bar',
-							label: {
-								normal: {
-									show: true,
-									position: 'inside'
-								}
-							},
-							data: [300, 270, 340, 344, 300, 320, 310],
-						},
-						{
-							name: '正面',
-							type: 'bar',
-							stack: '总量',
-							label: {
-								normal: {
-									show: true
-								}
-							},
-							data: [120, 102, 141, 174, 190, 250, 220]
-						},
-						{
-							name: '负面',
-							type: 'bar',
-							stack: '总量',
-							label: {
-								normal: {
-									show: true,
-									position: 'left'
-								}
-							},
-							data: [-20, -32, -21, -34, -90, -130, -110]
-						}
-					]
-				},
+				option1: null,
+				option2: null,
+				option3:null,
+				dat: null,
+				// getrisk 请求服务器得到感染风险
+				getrisk:0.2,
+				// getzz 请求服务器得到用于填写的症状数据
+				getzz:[100, 30, 30, 100, 30, 30, 100, 100],
+				zz:[100, 25, 100, 25, 25, 100, 100, 25],
+				risk:0.1,
+				zzrisk:[8.7, 13.6, 13.9, 18.6, 38.1, 50, 67.7, 87.9],
+				exist:[],
+				notexist:[],
+				datashow:[]
 			}
 		},
-	methods: {
-		chart1init() {
-			this.$refs.chart1.init(echarts, chart => {
-				chart.setOption(this.option1);
-			});
+		onLoad(value) {
+			// onLoad函数中获取结果,并在这里设置值
+			console.log('onLoad');
+			this.processChart2();
+			this.processChart3();
 		},
-		chart2init() {
-		this.$refs.chart2.init(echarts, chart => {
-			chart.setOption(this.option1);
-		});
+		methods: {
+			toHome()
+			{
+				console.log('返回');
+				uni.switchTab({
+					url: '/pages/index/index'
+				});
+			},
+			processChart1(value) {
+				// 设置水球的属性
+				 if(value < 0.25){
+				    return '#00FF00';
+				  }
+				  else if (value < 0.5){
+				    return '#4DBE61';
+				  }
+				  else if (value < 0.75){
+				    return '#FDB72F';
+				  }
+				  else{
+				    return '#FF0000';
+				  }
+			},
+			processChart2() {
+				// 雷达图处理
+				return;
+			},
+			processChart3() {
+				// 柱状图参数初始化
+				for (let i = 0; i < 8; i++) {
+				  if (this.zz[i] == 100) {
+				    this.datashow.push({
+				      value: this.zzrisk[i],
+				      itemStyle: {
+				        color: '#FDB72F'
+				      }
+				    })
+				    this.exist.push(this.datashow[i]);
+				  } else {
+				    this.datashow.push({
+				      value: this.zzrisk[i],
+				      itemStyle: {
+				        color: '#4DBE61'
+				      }
+				    });
+				    this.notexist.push(this.datashow[i]);
+				  }
+				}
+			},
+			setchart1Option(value) {
+				this.option1 = {
+				    title: {
+				      text: '健康风险',
+				    },
+				    grid:{
+				      left: 0,
+				      top: 0,
+				      bottom: 0
+				  },
+				  series: [{
+				      type: 'liquidFill',
+				      waveAnimation: true,
+				      animation: true,
+				      // color: '#ff8',
+				      radius: '95%',
+				      center: ['50%', '50%'],
+				      data: [
+				        {
+				          value: value,
+				          direction: "left",
+				          itemStyle: {
+				            normal: {
+				            //这里就是根据不同的值显示不同球体的颜色
+				              color: this.processChart1(value),
+				            },
+				          },
+				        },
+				      ],
+				      label: {
+				        formatter: value,    //  值
+				        fontSize: 20,
+				        fontFamily: 'Lobster Two',
+				      },
+				      backgorundStyle:{
+				        color:this.processChart1(value)
+				      },
+				      outline: {
+				        borderDistance: 0,
+				        itemStyle: {
+				          borderWidth: 5,
+				          borderColor: {
+				            type: 'linear',
+				            x: 0,
+				            y: 0,
+				            x2: 0,
+				            y2: 1,
+				            colorStops: [{
+				              offset: 0,
+				              color: 'rgba(69, 73, 240, 0)'
+				            }, {
+				              offset: 0.5,
+				              color: 'rgba(69, 73, 240, .25)'
+				            }, {
+				              offset: 1,
+				              color: 'rgba(69, 73, 240, 1)'
+				            }],
+				            globalCoord: false
+				          },
+				          shadowBlur: 10,
+				          shadowColor: '#ffffff',
+				        }
+				      },
+				  }]
+				  };
+			},
+			setchart2Option() {
+				this.option2 = {
+				    color: ['#67F9D8', '#FFE434', '#56A3F1', '#FF917C'],
+				    title: {
+				      text: '我的症状'
+				    },
+				    legend: {},
+				    radar: [
+				      {
+				        indicator: [
+				          { text: '发烧', max: 100},
+				          { text: '气喘', max: 100},
+				          { text: '身体乏力', max: 100 },
+				          { text: '咽干咽痛', max: 100 },
+				          { text: '咽痒咳嗽', max: 100 },
+				          { text: '肠胃不适', max: 100},
+				          { text: '流涕鼻塞', max: 100},
+				          { text: '头痛', max: 100},
+				        ],
+				        center: ['50%', '50%'],
+				        radius: 70,
+				        startAngle: 90,
+				        splitNumber: 4,
+				        shape: 'circle',
+				        axisName: {
+				          formatter: '[{value}]',
+				          color: '#428BD4'
+				        },
+				        splitArea: {
+				          areaStyle: {
+				            // ['#FFFFFF', '#26C3BE', '#64AFE9', '#428BD4'],
+				            color: ['#FFFFFF', '#4DBE61', '#FDB72F', '#FF0000'],
+				            shadowColor: 'rgba(0, 0, 0, 0.2)',
+				            shadowBlur: 10
+				          }
+				        },
+				        axisLine: {
+				          lineStyle: {
+				            color: 'rgba(211, 253, 250, 0.8)'
+				          }
+				        },
+				        splitLine: {
+				          lineStyle: {
+				            color: 'rgba(211, 253, 250, 0.8)'
+				          }
+				        }
+				      }
+				    ],
+				    series: [
+				      {
+				        type: 'radar',
+				        emphasis: {
+				          lineStyle: {
+				            width: 4
+				          }
+				        },
+				        data: [
+				          {
+				            value: this.zz,
+				            areaStyle: {
+				              color: 'rgba(255, 228, 52, 0.6)'
+				            }
+				          }
+				        ]
+				      }
+				    ]
+				  };
+			},
+			setchaty3Option()
+			{
+				this.option3 = {
+				    title: {
+				      text: '感染者症状频率'
+				    },
+				    grid:{
+				      left: '20%',
+				      right: '15%'
+				    },
+				    xAxis: {
+				      name:'频率',
+				      type: 'value'
+				    },
+				    yAxis: {
+				      name: '症状',
+				      type: 'category',
+				      data: [
+				        '肠胃不适',
+				        '头痛',
+				        '咽干咽痛',
+				        '气喘',
+				        '身体乏力',
+				        '流涕鼻塞',
+				        '咽痒咳嗽',
+				        '发烧'
+				      ]
+				    },
+				    series: [
+				      {
+				        data: this.datashow,
+				        type: 'bar',
+				        label: {
+				          show: true,
+				          precision: 1,
+				          position: 'right',
+				          formatter: '{c}%'
+				        }
+				      },
+				      
+				    ]
+				  };
+			},
+			setchart3Option()
+			{
+				 this.option3 = {
+				    title: {
+				      text: '感染者症状频率'
+				    },
+				    grid:{
+				      left: '20%',
+				      right: '15%'
+				    },
+				    xAxis: {
+				      name:'频率',
+				      type: 'value'
+				    },
+				    yAxis: {
+				      name: '症状',
+				      type: 'category',
+				      data: [
+				        '肠胃不适',
+				        '头痛',
+				        '咽干咽痛',
+				        '气喘',
+				        '身体乏力',
+				        '流涕鼻塞',
+				        '咽痒咳嗽',
+				        '发烧'
+				      ]
+				    },
+				    series: [
+				      {
+				        data: this.datashow,
+				        type: 'bar',
+				        label: {
+				          show: true,
+				          precision: 1,
+				          position: 'right',
+				          formatter: '{c}%'
+				        }
+				      },
+				      
+				    ]
+				  };
+			},
+			chart1init() {
+				this.$refs.chart1.init(echarts, chart => {
+					console.log('finished')
+					this.setchart1Option(this.risk);
+					chart.setOption(this.option1);
+				});
+			},
+			chart2init() {
+				this.$refs.chart2.init(echarts, chart => {
+					this.setchart2Option();
+					chart.setOption(this.option2);
+				});
+			},
+			chart3init(){
+				this.$refs.chart3.init(echarts, chart => {
+					this.processChart3();
+					this.setchart3Option();
+					chart.setOption(this.option3);
+				});
+			}
 		}
-	}
 	}
 </script>
 
-<style>
-	.echart_panel1 {
-		widht: 100%;
-		height: 600rpx
+<style scoped>
+	.result-container {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		margin: 20rpx auto;
+		width: 95vw;
 	}
+	.back-button {
+		width:80vw;
+		margin: 30rpx auto;
+	}
+
 </style>
--- covid-control/pages/recoveryresult/recoveryresult.vue
+++ covid-control/pages/recoveryresult/recoveryresult.vue
@@ -1,22 +1,589 @@
 <template>
-	<view>
-		
+	<view class="result-container">
+		<view class="echart_panel1">
+			<l-echart ref="chart1" @finished="chart1init"></l-echart>
+		</view>
+		<view class="echart_panel2">
+			<l-echart ref="chart2" @finished="chart2init"></l-echart>
+		</view>
+		<button class="back-button" @click="toHome">返回首页</button>
 	</view>
 </template>
 
 <script>
+	import * as echarts from '@/uni_modules/lime-echart/static/echarts.min';
 	export default {
 		data() {
 			return {
-				
+				temp_min: null,
+				temp_max: null,
+				schema: [],
+				my_state: null,
+				ave_state: null,
+				option1: null,
+				option2: null,
+				dat: null,
 			}
 		},
+		onLoad(value) {
+			// onLoad函数中获取结果,并在这里设置值
+			console.log('onLoad');
+			this.processChart1();
+			this.processChart2();
+		},
 		methods: {
-			
+			toHome()
+			{
+				console.log('返回');
+				uni.switchTab({
+					url: '/pages/index/index'
+				});
+			},
+			processChart1() {
+				this.temp_min = 35;
+				this.temp_max = 43;
+				let data_get = {
+					"msg": "query succeed",
+					"status": 1,
+					"value": {
+						"2022-12-14": { // 6
+							"_id": "6398856a9f4ef3708338f4a6",
+							"date": [2022, 12, 14],
+							"days_symp": 0,
+							"location": "",
+							"nc_test": null,
+							"stamp": 1670940010.275769,
+							"symptom": {
+								"cw": false,
+								"fl": false,
+								"ks": false,
+								"lt": false,
+								"qc": false,
+								"tt": true,
+								"yt": false
+							},
+							"temp": 36.7,
+							"uuid": 1
+						},
+						"2022-12-07": { // 1
+							"_id": "63986a089f4ef3708338f4a5",
+							"date": [2022, 12, 7],
+							"days_symp": 0,
+							"location": "",
+							"nc_test": null,
+							"stamp": 1670933000.8822377,
+							"symptom": {
+								"cw": false,
+								"fl": false,
+								"ks": false,
+								"lt": false,
+								"qc": false,
+								"tt": true,
+								"yt": false
+							},
+							"temp": 36.7,
+							"uuid": 1
+						},
+						"2022-12-08": { // 2
+							"_id": "6398856a9f4ef3708338f4a6",
+							"date": [2022, 12, 8],
+							"days_symp": 0,
+							"location": "",
+							"nc_test": null,
+							"stamp": 1670940010.275769,
+							"symptom": {
+								"cw": false,
+								"fl": false,
+								"ks": false,
+								"lt": false,
+								"qc": false,
+								"tt": true,
+								"yt": false
+							},
+							"temp": 36.7,
+							"uuid": 1
+						},
+						"2022-12-10": { // 3
+							"_id": "6398856a9f4ef3708338f4a6",
+							"date": [2022, 12, 10],
+							"days_symp": 0,
+							"location": "",
+							"nc_test": null,
+							"stamp": 1670940010.275769,
+							"symptom": {
+								"cw": false,
+								"fl": false,
+								"ks": false,
+								"lt": false,
+								"qc": false,
+								"tt": true,
+								"yt": false
+							},
+							"temp": 36.7,
+							"uuid": 1
+						},
+						"2022-12-11": { // 4
+							"_id": "6398856a9f4ef3708338f4a6",
+							"date": [2022, 12, 11],
+							"days_symp": 0,
+							"location": "",
+							"nc_test": null,
+							"stamp": 1670940010.275769,
+							"symptom": {
+								"cw": false,
+								"fl": false,
+								"ks": false,
+								"lt": false,
+								"qc": false,
+								"tt": true,
+								"yt": false
+							},
+							"temp": 36.7,
+							"uuid": 1
+						},
+						"2022-12-13": { // 5
+							"_id": "6398856a9f4ef3708338f4a6",
+							"date": [2022, 12, 13],
+							"days_symp": 0,
+							"location": "",
+							"nc_test": null,
+							"stamp": 1670940010.275769,
+							"symptom": {
+								"cw": false,
+								"fl": false,
+								"ks": false,
+								"lt": false,
+								"qc": false,
+								"tt": true,
+								"yt": false
+							},
+							"temp": 36.7,
+							"uuid": 1
+						},
+						"2023-1-1": { // 7
+							"_id": "6398856a9f4ef3708338f4a6",
+							"date": [2023, 1, 1],
+							"days_symp": 0,
+							"location": "",
+							"nc_test": null,
+							"stamp": 1670940010.275769,
+							"symptom": {
+								"cw": false,
+								"fl": false,
+								"ks": false,
+								"lt": false,
+								"qc": false,
+								"tt": true,
+								"yt": false
+							},
+							"temp": 36.7,
+							"uuid": 1
+						}
+					}
+				}
+				let date_list = []
+				for (let i in data_get.value) {
+					date_list.push(data_get.value[i].date)
+				}
+				date_list.sort(function(a, b) {
+					if (a[0] != b[0]) {
+						return a[0] - b[0]
+					} else if (a[1] != b[1]) {
+						return a[1] - b[1]
+					} else {
+						return a[2] - b[2]
+					}
+				}); // 日期升序排序
+				// 挑出七日体温时间范围，这块代码有点麻烦，晚会再补充
+				let date_start = date_list[0]
+				let date_end = []
+				let date_end_year = date_start[0]
+				let date_end_month = date_start[1]
+				let date_end_day = date_start[2] + 6
+
+				function isLeapYear(year) {
+					return ((year % 400 === 0) || (year % 100 !== 0 && year % 4 === 0))
+				}
+				if (date_end_month > 12) {
+					date_end_month = date_end_month % 12
+					date_end_year += 1
+				}
+				date_end = [date_end_year, date_end_month, date_end_day]
+				let tmp = []
+				for (let i = 0; i < date_list.length & i < 7; i++) { // 这块加一个时间范围比较
+					if (date_list[i][1] > 9 && date_list[i][2] > 9) {
+						tmp.push(date_list[i][0] + '-' + date_list[i][1] + '-' + date_list[i][2])
+					}
+					if (date_list[i][1] > 9 && date_list[i][2] <= 9) {
+						tmp.push(date_list[i][0] + '-' + date_list[i][1] + '-0' + date_list[i][2])
+					}
+					if (date_list[i][1] <= 9 && date_list[i][2] > 9) {
+						tmp.push(date_list[i][0] + '-0' + date_list[i][1] + '-' + date_list[i][2])
+					}
+					if (date_list[i][1] <= 9 && date_list[i][2] <= 9) {
+						tmp.push(date_list[i][0] + '-0' + date_list[i][1] + '-0' + date_list[i][2])
+					}
+				}
+				date_list = tmp
+				//console.log(date_list)
+				let my_state_tmp = []
+				for (let i = 0; i < date_list.length; i++) {
+					my_state_tmp.push([i, data_get.value[date_list[i]]])
+				}
+				console.log(my_state_tmp)
+				this.my_state = [
+					[0, 36.8, '阳', 1, 0, 0, 0, 0],
+					[1, 36.8, '阳', 1, 0, 0, 0, 0],
+					// [2, 39.1, '阳', 1, 1, 0, 0, 0],
+					[3, 41.5, '阳', 0, 0, 1, 0, 0],
+					[4, 38.5, '阳', 0, 0, 0, 1, 0],
+					[5, 36.8, '阳', 0, 0, 0, 0, 1],
+					[6, 36.8, '阳', 1, 0, 0, 0, 1],
+				]
+				this.ave_state = [
+					[0, 36.8, '阳', 1, 1, 0, 0, 0],
+					[1, 37.3, '阳', 1, 1, 0, 0, 0],
+					[2, 39, '阳', 1, 1, 1, 0, 1],
+					[3, 40, '阳', 1, 1, 1, 1, 1],
+					[4, 37.5, '阳', 1, 1, 1, 1, 1],
+					[5, 37.3, '阳', 1, 1, 1, 1, 1],
+					[6, 36.8, '阴', 0, 0, 0, 0, 0],
+				];
+				this.schema = [{
+						name: 'date',
+						index: 0,
+						text: '日'
+					},
+					{
+						name: 'temp',
+						index: 1,
+						text: '体温'
+					},
+					{
+						name: 'test_res',
+						index: 2,
+						text: '核酸结果'
+					},
+					{
+						name: 'throat',
+						index: 3,
+						text: '咽干咽痛'
+					},
+					{
+						name: 'weakness',
+						index: 4,
+						text: '身体乏力'
+					},
+					{
+						name: 'cough',
+						index: 5,
+						text: '咽痒咳嗽'
+					},
+					{
+						name: 'nose',
+						index: 6,
+						text: '流涕鼻塞'
+					},
+					{
+						name: 'stomach',
+						index: 7,
+						text: '肠胃不适'
+					},
+					{
+						name: 'headache',
+						index: 8,
+						text: '头痛'
+					},
+					{
+						name: 'asthma',
+						index: 9,
+						text: '气喘'
+					}
+				];
+
+				this.itemStyle = {
+					// item 阴影设置
+					// opacity: 0.8,
+					shadowBlur: 50,
+					shadowOffsetX: 0,
+					shadowOffsetY: 0,
+					shadowColor: 'rgba(0,0,0,0.2)',
+					borderColor: '#fff',
+					borderWidth: 100
+				};
+			},
+			processChart2() {
+				var my_state = [
+					[0, 36.8, '阳', 1, 0, 0, 0, 0, 1, 0],
+					[1, 36.8, '阳', 1, 0, 0, 0, 0, 1, 0],
+					// [2, 39.1, '阳', 1, 1, 0, 0, 0, 1, 0],
+					[3, 41.5, '阳', 0, 0, 1, 0, 0, 1, 0],
+					[4, 37.5, '阳', 0, 0, 0, 1, 0, 1, 0],
+				 [5, 37.3, '阳', 0, 0, 0, 0, 1, 1, 0],
+					[6, 36.8, '阳', 1, 0, 0, 0, 1, 1, 1],
+				]
+
+				const contrast = [
+					['轻微', '加重', '加剧', '持续', '持续', '减轻', '明显好转'], // 咽干咽痛
+					['轻微', '加重', '加剧', '持续', '持续', '减轻', '明显好转'], // 身体乏力
+					['尚无', '尚无', '出现', '出现', '持续', '加重', '明显好转'], // 咽痒咳嗽
+					['尚无', '尚无', '尚无', '出现', '持续', '加重', '明显好转'], // 流涕鼻塞
+					['尚无', '尚无', '出现', '出现', '减轻', '减轻', '明显好转'], // 肠胃不适
+					['阳性', '阳性', '阳性', '阳性', '阳性', '可能转阴', '很大可能转阴'] // 核酸检测
+				].map(function(item) {
+					return [item[0], item[1], item[2], item[3], item[4], item[5], item[6]];
+				});
+
+				this.dat = []; // 生成格子图中的数据
+				for (var i = 0; i < my_state.length; i++) { // 第几天
+					for (var j = 3; j < my_state[i].length - 2; j++) { // 症状
+						if (my_state[i][j] == 1) {
+							this.dat.push([my_state[i][0], j - 3, my_state[i][j], contrast[j - 3][i], contrast[5][i]])
+						}
+					}
+				}
+				this.dat.map(function(item) {
+					return [item[0], item[1], item[2]];
+				});
+			},
+			setchart1Option() {
+				this.option1 = {
+					color: ['#377E47', '#FF6159', ], // 点的颜色,不会调成实心的
+					legend: {
+						top: 10,
+						data: ['我的病程', '平均病程'],
+						textStyle: {
+							fontSize: 16
+						}
+					},
+					grid: { // 表格布局位置
+						left: '10%',
+						right: '15%',
+						top: '20%',
+						bottom: '8%'
+					},
+					tooltip: {
+						backgroundColor: 'rgba(255,255,255,0.7)',
+						position: function(pos, params, dom, rect, size) {
+							// 鼠标在左侧时 tooltip 显示到右侧，鼠标在右侧时 tooltip 显示到左侧。
+							var obj = {
+								top: 60
+							};
+							obj[['left', 'right'][+(pos[0] < size.viewSize[0] / 2)]] = 5;
+							return obj;
+						},
+						formatter:(param)=>{
+							let value = param.value;
+							// prettier-ignore
+							
+							return this.schema[2].text + '：' + value[2] + '性' + '\n' +
+								this.schema[3].text + '：' + (value[3] == 1 ? '是' : '否') + '\n' +
+								this.schema[4].text + '：' + (value[4] == 1 ? '是' : '否') + '\n' +
+								this.schema[5].text + '：' + (value[5] == 1 ? '是' : '否') + '\n' +
+								this.schema[6].text + '：' + (value[6] == 1 ? '是' : '否') + '\n' +
+								this.schema[7].text + '：' + (value[7] == 1 ? '是' : '否')
+							
+							// const schema = [
+							//   { name: 'date', index: 0, text: '日' },
+							//   { name: 'temp', index: 1, text: '体温' },
+							//   { name: 'test_res', index: 2, text: '核酸结果' },
+							//   { name: 'throat', index: 3, text: '咽干咽痛' },
+							//   { name: 'weakness', index: 4, text: '身体乏力' },
+							//   { name: 'cough', index: 5, text: '咽痒咳嗽' },
+							//   { name: 'nose', index: 6, text: '流涕鼻塞' },
+							//   { name: 'stomach', index: 7, text: '肠胃不适' },
+							//   { name: 'headache', index: 8, text: '头痛' },
+							//   { name: 'asthma', index: 9, text: '气喘' },
+							// ];
+						}
+					},
+					xAxis: {
+						type: 'category',
+						name: '日期',
+						data: [
+							'第1天', '第2天', '第3天', '第4天', '第5天', '第6天', '第7天',
+						],
+						nameTextStyle: {
+							fontSize: 16 // 日期两个字的大小
+						},
+						splitArea: { // 竖着的条纹
+							show: true
+						},
+					},
+					yAxis: {
+						type: 'value',
+						name: '体温',
+						nameLocation: 'end',
+						nameTextStyle: {
+							fontSize: 16
+						},
+						splitLine: {
+							show: true
+						},
+						min: this.temp_min
+					},
+					visualMap: {
+						show: false,
+						calculable: false,
+						dimension: 1,
+						min: this.temp_min,
+						max: this.temp_max,
+						itemWidth: 30,
+						itemHeight: 50,
+						precision: 0.1,
+						text: ['圆形大小：体温'],
+						textGap: 30,
+						inRange: {
+							symbolSize: [10, 50]
+						},
+						outOfRange: {
+							symbolSize: [10, 70],
+							color: ['rgba(255,255,255,0.4)']
+						},
+						controller: {
+							inRange: {
+								color: ['#C3C3C3']
+							},
+							outOfRange: {
+								color: ['#999']
+							}
+						}
+					},
+					series: [{
+							name: '平均病程',
+							type: 'line',
+							itemStyle: this.itemStyle,
+							data: this.ave_state
+						},
+						{
+							name: '我的病程',
+							type: 'line',
+							itemStyle: this.itemStyle,
+							data: this.my_state
+						},
+					]
+				}
+			},
+			setchart2Option() {
+				this.option2 =  {
+					tooltip: {
+						backgroundColor: 'rgba(255,255,255,0.7)',
+						formatter: function(param) {
+							var value = param.value;
+							return '其他患者在今天：\n'
+								// + value[0] + ' ' + value[1] + ' ' + value[2] + ' ' 
+								+
+								'该症状' + value[3] + '\n' +
+								'核酸检测结果' + value[4]
+						}
+					},
+					grid: {
+						left: '10%',
+						right: '15%',
+						top: '5%',
+						bottom: '10%'
+					},
+					xAxis: {
+						type: 'category',
+						name: '日期',
+						data: [
+							'第1天', '第2天', '第3天', '第4天', '第5天', '第6天', '第7天',
+						],
+						splitArea: {
+							show: true // 竖着的条纹
+						},
+						nameTextStyle: {
+							fontSize: 16 // 日期两个字的大小
+						},
+						textStyle: { // 没用，不会调
+							fontSize: 1600000
+						},
+						// position: 'top'
+					},
+					yAxis: {
+						type: 'category',
+						name: '出现症状',
+						data: [
+							'咽干\n咽痛', '身体\n乏力', '咽痒\n咳嗽', '流涕\n鼻塞', '肠胃\n不适',
+						],
+						nameTextStyle: {
+							fontSize: 16 // 出现症状4个字的大小
+						},
+						splitArea: {
+							// show: true // 横着的条纹
+						},
+						axisLabel: {
+							fontSize: 12,
+						},
+						offset: 1,
+					},
+					visualMap: {
+						show: false,
+						calculable: false,
+						orient: 'horizontal',
+						inRange: {
+							// color: ['#EB3324']
+							// color: ['#77FF90', '#377E47', '#F09B59', '#EB3324']
+						}
+					},
+					series: [{
+						name: 'Punch Card',
+						type: 'heatmap',
+						// data: [dat, contrast],
+						data: this.dat,
+						label: {
+							show: false // 控制是否显示数字
+						},
+						itemStyle: {
+							shadowBlur: 10, // 这块控制阴影
+							shadowColor: 'rgba(25, 100, 150, 0.5)',
+							shadowOffsetY: 5,
+							color: new echarts.graphic.RadialGradient(0.5, 0.2, 0.9, [ // 这块控制颜色
+								{
+									offset: 0,
+									color: 'rgb(245, 96, 81)'
+								},
+								{
+									offset: 0,
+									color: 'rgb(215, 66, 51)'
+								}
+							]),
+						}
+
+					}]
+				};
+			},
+			chart1init() {
+				this.$refs.chart1.init(echarts, chart => {
+					console.log('finished')
+					this.setchart1Option();
+					chart.setOption(this.option1);
+				});
+			},
+			chart2init() {
+				this.$refs.chart2.init(echarts, chart => {
+					this.setchart2Option();
+					chart.setOption(this.option2);
+				});
+			}
 		}
 	}
 </script>
 
-<style>
-
+<style scoped>
+	.back-button {
+		width:80vw;
+		margin: 30rpx auto;
+	}
+	.result-container {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-around;
+		margin: 20rpx auto;
+		width: 95vw;
+	}
+	.echart_panel1 {
+		widht: 100%;
+		height: 600rpx
+	}
 </style>
