bdf6fc3fb379f187908298a4e8770e6ca37f6993
charming-c <822170415@qq.com>
2022-12-15 18:00:25 +0800

    handbook page handle

---
+++ covid-control/pages/recoverymanual/components/DetailTip.vue
@@ -0,0 +1,28 @@
+<template>
+	<view style="width: 100%; height: 100%;">
+		<u-image :fade="true" height="100%" duration="450" :src="src" mode="widthFix">
+			<u-loading mode="flower" size="36" slot="loading"></u-loading>
+		</u-image>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "DetailTip",
+		data() {
+			return {
+
+			}
+		},
+		props: {
+			src: String,
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ covid-control/pages/recoverymanual/tipSign.vue
@@ -0,0 +1,31 @@
+<template>
+	<view style="width: 100%; height: 100%;">
+		<DetailTip :src="imgSrc"></DetailTip>
+	</view>
+</template>
+
+<script>
+	import DetailTip from './components/DetailTip.vue'
+	export default {
+
+		data() {
+			return {
+				imgSrc: '',
+			}
+		},
+		onLoad() {
+			this.imgSrc = uni.getStorageSync('index');
+			console.log(this.imgSrc);
+		},
+		components: {
+			DetailTip
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
